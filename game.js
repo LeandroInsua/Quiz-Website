1cdc5022ec0fa4ccf41f931c784a207060e93396
Leo <leandrohinsua@gmail.com>
2026-03-25 23:08:42 -0300

    bug

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
-/* const BASE_PATH = window.location.hostname.includes('github.io') 
-    ? 'leandroinsua.github.io//'   // Change only this if your repo name ever changes
-    : '/';               // Local development */
+const BASE_PATH = window.location.hostname.includes('github.io') 
+    ? '/Quiz-Website/'   // Change only this if your repo name ever changes
+    : '/';               // Local development
 
 /* QUIZ OPTIONS */
 const question = document.getElementById("question");
@@ -29,7 +29,7 @@ let progressBarFill = document.getElementById("progress-bar-fill");
 
 //LOAD DATA, THEN START
 async function loadKanji() {
-    const response = await fetch(`https://leandroinsua.github.io/Quiz-Website/Quiz-Website/data/kanji_data_N${jlptLevel}.json`); // document containing kanji
+    const response = await fetch(`https://github.com/LeandroInsua/Quiz-Website/tree/main/Data/kanji_data_N${jlptLevel}.json`); // document containing kanji
     if (!response.ok) throw new Error("Failed to load json");
     const data = await response.json();
 
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-/* const BASE_PATH = window.location.hostname.includes('github.io') 
-    ? 'leandroinsua.github.io/Quiz-Website/'   // Change only this if your repo name ever changes
-    : '/';               // Local development */
+const BASE_PATH = window.location.hostname.includes('github.io') 
+    ? '/Quiz-Website/'   // Change only this if your repo name ever changes
+    : '/';               // Local development
 
 /* Layers */
 const layers = {
@@ -46,7 +46,7 @@ function showOnly(layerKey) {
 async function loadKanji(jlpt) {
     if (!kanjiCache[jlpt]) {
          try {
-            const response = await fetch(`https://leandroinsua.github.io/Quiz-Website/Quiz-Website/data/kanji_data_N${jlpt}.json`);
+            const response = await fetch(`https://github.com/LeandroInsua/Quiz-Website/tree/main/Data/kanji_data_N${jlpt}.json`);
             if (!response.ok) {
                 throw new Error("Failed to fetch Kanji data");
             }
